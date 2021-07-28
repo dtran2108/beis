972cd8452d3be00ff39b3aeb58ac4d9daa3eb2c3
Tran Duong <dtran2108@gmail.com>
2021-07-28 13:55:52 +0700

    add 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import Meta from '~/views/SEO/meta';
+import Home from '~/container/Home/index';
+import HOCApp from '~/views/HOCApp';
 
-import { Result, Button } from 'antd';
-import Router from 'next/router';
-
-export default function Custom404() {
+export default function Index() {
   return (
-    <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={
-        <Button type="primary" onClick={() => Router.router.push('/')}>
-          Back Home
-        </Button>
-      }
-    />
+    <HOCApp>
+      <Meta
+        title={'404 Not Found - Beis'}
+        image={'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png'}
+        ogImage={'https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png'}
+        description={
+          'The best travel gear and accessories for the modern traveler. These are perfect for weekend sleepovers, beach days, and summers in the south of France. Designed and created by Shay Mitchell.'
+        }
+      />
+      <center>
+        <div>
+          <br />
+          <img
+            alt="beis"
+            src="https://cdn.shopify.com/s/files/1/0032/3423/4479/files/Logo_300x168_small_30eb8a98-7569-4e8b-8c1c-880fbddbe46b.png?6227089005472032858"
+          />
+          <br />
+          <h5 className="mb-0">404 Page Not Found</h5>
+          <p className="mb-0">
+            The page you requested does not exist. Click <a href="/">here</a> to continue shopping.
+          </p>
+          <br />
+          <br />
+          <br />
+          <br />
+        </div>
+      </center>
+    </HOCApp>
   );
 }
